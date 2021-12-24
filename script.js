55d36486c56369e17fe68e867afd1cecde1f1d90
millieof2017 <61999030+millieof2017@users.noreply.github.com>
2021-12-23 20:40:23 -0500

    edits pt2

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function myFunction() {
   // If the checkbox is checked, display the output text
   if (checkBox.checked == true){
     text.style.opacity = "100%";
-    $(text).attr('href','mailto:email@example.com?subject=Queer%20childhood%20submission');
+    $(text).attr('href','mailto:Qu33rchildh00d@gmail.com?subject=Queer%20childhood%20submission');
 
   } else {
     text.style.opacity = "40%";
